4d91d0e1d4784482aa14cd4412743e83c5995a99
Jörg Thalheim <joerg@thalheim.io>
2023-08-12 06:27:07 +0200

    sevdesk-invoicer: convert to pyproject.toml

--- sevdesk-invoicer.nix
+++ sevdesk-invoicer.nix
@@ -6,10 +6,12 @@ pkgs.python3.pkgs.buildPythonApplication {
   pname = "sevdesk-invoicer";
   version = "0.0.1";
   src = ./sevdesk-invoicer;
+  format = "pyproject";
   propagatedBuildInputs = [
     sevdesk
   ];
   nativeBuildInputs = [
+    pkgs.python3.pkgs.setuptools
     pkgs.python3.pkgs.black
     pkgs.python3.pkgs.mypy
   ];
